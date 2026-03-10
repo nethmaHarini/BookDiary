e93f7b16234e405f4f6c405216e11cebd7956f1c
ashin <ashinsaparami2002@gmail.com>
2026-03-10 23:38:42 +0530

    Initial commit

--- app/src/main/java/com/example/favoriteuserinterface/MainActivity.java
+++ app/src/main/java/com/example/favoriteuserinterface/MainActivity.java
@@ -1,24 +1,66 @@
 package com.example.favoriteuserinterface;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    ArrayList<Book> bookList;
+    BookAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // connect xml layout
+        setContentView(R.layout.activity_favorites);
+
+        // connect RecyclerView
+        recyclerView = findViewById(R.id.bookRecycler);
+
+        // set layout manager
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // create array list
+        bookList = new ArrayList<>();
+
+        // add books
+        bookList.add(new Book(
+                "The Great Gatsby",
+                "F. Scott Fitzgerald",
+                "4.8",
+                R.drawable.gatsby
+        ));
+
+        bookList.add(new Book(
+                "1984",
+                "George Orwell",
+                "4.9",
+                R.drawable.book1984
+        ));
+
+        bookList.add(new Book(
+                "Dune",
+                "Frank Herbert",
+                "4.7",
+                R.drawable.dune
+        ));
+
+        bookList.add(new Book(
+                "Atomic Habits",
+                "James Clear",
+                "5.0",
+                R.drawable.atomic
+        ));
+
+        // set adapter
+        adapter = new BookAdapter(this, bookList);
+        recyclerView.setAdapter(adapter);
     }
 }
